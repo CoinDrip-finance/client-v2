7567ff75fc517c3299be98808c15c16bbda1eace
Razvan Statescu <razvan@statescu.net>
2023-10-09 12:06:45 +0300

    update action popups design

--- components/stream_details/CancelPopup.tsx
+++ components/stream_details/CancelPopup.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from '@elrond-giants/erd-react-hooks/dist';
-import { KeyIcon } from '@heroicons/react/24/outline';
+import { ArrowDownTrayIcon, BanknotesIcon, ChartPieIcon, KeyIcon, WalletIcon } from '@heroicons/react/24/outline';
 import axios from 'axios';
 import { useEffect, useMemo, useState } from 'react';
 
@@ -9,8 +9,8 @@ import { IStreamResponse } from '../../types';
 import StreamingContract from '../../utils/contracts/streamContract';
 import { denominate } from '../../utils/economics';
 import { formatNumber, getAmountStreamed, getClaimedAmount, getDepositAmount } from '../../utils/presentation';
-import ProgressBarSmall from '../stream_list/ProgressBarSmall';
 import StreamDetailsBasePopup from './PopupBase';
+import StreamPropItem from './StreamPropItem';
 
 interface CancelPopupProps {
   data: IStreamResponse;
@@ -75,10 +75,38 @@ export default function CancelPopup({ data, open, onClose }: CancelPopupProps) {
       submitButtonLabel="Cancel"
     >
       <div>
+        <div className="grid grid-cols-2 gap-x-4 gap-y-6">
+          <StreamPropItem
+            label="Deposited"
+            value={`${deposited} ${data.stream.payment.token_name}`}
+            Icon={WalletIcon}
+          />
+          <StreamPropItem
+            label="Streamed"
+            value={`${formatNumber(amountStreamed.value)} ${data.stream.payment.token_name}`}
+            Icon={ChartPieIcon}
+          />
+          <StreamPropItem
+            label="Withdrawn"
+            value={`${formatNumber(claimed.value)} ${data.stream.payment.token_name}`}
+            Icon={ArrowDownTrayIcon}
+          />
+          <StreamPropItem
+            label="Withdrawable by recipient"
+            value={`${formatNumber(readyToClaimByRecipient)} ${data.stream.payment.token_name}`}
+            Icon={BanknotesIcon}
+          />
+          <StreamPropItem
+            label="Withdrawable by sender"
+            value={`${formatNumber(readyToClaimBySender)} ${data.stream.payment.token_name}`}
+            Icon={BanknotesIcon}
+          />
+        </div>
+
         {streamRecipient !== address && data?.stream?.sender !== address && (
-          <div className="bg-neutral-800 py-2 px-4 rounded-lg text-orange-400 text-sm">
-            <div className="flex items-center font-medium">
-              <KeyIcon className="w-4 h-4 mr-2" />
+          <div className="bg-neutral-800 border border-neutral-700 py-2 px-4 rounded-lg text-sm mt-8">
+            <div className="flex items-center font-semibold text-orange-400">
+              <KeyIcon className="w-5 h-5 mr-2" />
               Missing permissions
             </div>
             <p className="font-light mt-2">
@@ -86,33 +114,6 @@ export default function CancelPopup({ data, open, onClose }: CancelPopupProps) {
             </p>
           </div>
         )}
-
-        <ul className="mt-4">
-          <li>
-            Deposited: {formatNumber(deposited)} {data.stream.payment.token_name}
-          </li>
-          <li className="flex items-center">
-            Streamed: {formatNumber(amountStreamed.value)} {data.stream.payment.token_name}
-            <div className="mx-4">
-              <ProgressBarSmall value={amountStreamed.percent} />
-            </div>
-            {`${amountStreamed.percent}%`}
-          </li>
-          <li className="flex items-center">
-            Withdrawn: {formatNumber(claimed.value)} {data.stream.payment.token_name}
-            <div className="mx-4">
-              <ProgressBarSmall value={claimed.percent} />
-            </div>
-            {`${claimed.percent}%`}
-          </li>
-
-          <li className="mt-4">
-            Withdrawable by recipient: {formatNumber(readyToClaimByRecipient)} {data.stream.payment.token_name}
-          </li>
-          <li className="mt-4">
-            Withdrawable by sender: {formatNumber(readyToClaimBySender)} {data.stream.payment.token_name}
-          </li>
-        </ul>
       </div>
     </StreamDetailsBasePopup>
   );
--- components/stream_details/ClaimPopup.tsx
+++ components/stream_details/ClaimPopup.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from '@elrond-giants/erd-react-hooks/dist';
-import { KeyIcon } from '@heroicons/react/24/outline';
+import { ArrowDownTrayIcon, BanknotesIcon, ChartPieIcon, KeyIcon, WalletIcon } from '@heroicons/react/24/outline';
 import axios from 'axios';
 import { useEffect, useMemo, useState } from 'react';
 
@@ -9,8 +9,8 @@ import { IStreamResponse } from '../../types';
 import StreamingContract from '../../utils/contracts/streamContract';
 import { denominate } from '../../utils/economics';
 import { formatNumber, getAmountStreamed, getClaimedAmount, getDepositAmount } from '../../utils/presentation';
-import ProgressBarSmall from '../stream_list/ProgressBarSmall';
 import StreamDetailsBasePopup from './PopupBase';
+import StreamPropItem from './StreamPropItem';
 
 interface ClaimPopupProps {
   data: IStreamResponse;
@@ -71,10 +71,33 @@ export default function ClaimPopup({ data, open, onClose }: ClaimPopupProps) {
       submitButtonLabel="Withdraw"
     >
       <div>
+        <div className="grid grid-cols-2 gap-x-4 gap-y-6">
+          <StreamPropItem
+            label="Deposited"
+            value={`${deposited} ${data.stream.payment.token_name}`}
+            Icon={WalletIcon}
+          />
+          <StreamPropItem
+            label="Streamed"
+            value={`${formatNumber(amountStreamed.value)} ${data.stream.payment.token_name}`}
+            Icon={ChartPieIcon}
+          />
+          <StreamPropItem
+            label="Withdrawn"
+            value={`${formatNumber(claimed.value)} ${data.stream.payment.token_name}`}
+            Icon={ArrowDownTrayIcon}
+          />
+          <StreamPropItem
+            label="Withdrawable"
+            value={`${formatNumber(readyToClaim)} ${data.stream.payment.token_name}`}
+            Icon={BanknotesIcon}
+          />
+        </div>
+
         {streamRecipient !== address && (
-          <div className="bg-neutral-800 py-2 px-4 rounded-lg text-orange-400 text-sm">
-            <div className="flex items-center font-medium">
-              <KeyIcon className="w-4 h-4 mr-2" />
+          <div className="bg-neutral-800 border border-neutral-700 py-2 px-4 rounded-lg text-sm mt-8">
+            <div className="flex items-center font-semibold text-orange-400">
+              <KeyIcon className="w-5 h-5 mr-2" />
               Missing permissions
             </div>
             <p className="font-light mt-2">
@@ -91,30 +114,6 @@ export default function ClaimPopup({ data, open, onClose }: ClaimPopupProps) {
             </p>
           </div>
         )}
-
-        <ul className="mt-4">
-          <li>
-            Deposited: {deposited} {data.stream.payment.token_name}
-          </li>
-          <li className="flex items-center">
-            Streamed: {formatNumber(amountStreamed.value)} {data.stream.payment.token_name}
-            <div className="mx-4">
-              <ProgressBarSmall value={amountStreamed.percent} />
-            </div>
-            {`${amountStreamed.percent}%`}
-          </li>
-          <li className="flex items-center">
-            Withdrawn: {formatNumber(claimed.value)} {data.stream.payment.token_name}
-            <div className="mx-4">
-              <ProgressBarSmall value={claimed.percent} />
-            </div>
-            {`${claimed.percent}%`}
-          </li>
-
-          <li className="mt-4">
-            Withdrawable: {formatNumber(readyToClaim)} {data.stream.payment.token_name}
-          </li>
-        </ul>
       </div>
     </StreamDetailsBasePopup>
   );
