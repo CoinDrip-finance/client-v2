126f9ca0be50edd5133208732bbe18c4fe35b207
Razvan Statescu <razvan@statescu.net>
2023-10-10 15:18:03 +0300

    update meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
           <link rel="shortcut icon" href="/favicon.png" />
         </Head>
         <body>
